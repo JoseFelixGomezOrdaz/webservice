2b7f5facd5daf7b3128c04a5a9c35ddac0bfd373
JoseFelixGomezOrdaz <1717110617@utectulancingo.edu.mx>
2019-03-26 21:27:18 -0600

    Base de datos remota

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import web
-
+"""
 db_host = 'localhost'
 db_name = 'ferreteria_jfgo'
 db_user = 'ferreteria'
 db_pw = 'ferre.2019'
+"""
+db_host = 'gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'j9gk65oe7j5df2be'
+db_user = 'wneatin9hw8g62bh'
+db_pw = 'zgznjrvcv6n3nq4d'
 
 db = web.database(
     dbn='mysql',
